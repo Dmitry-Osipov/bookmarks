df0c6fc45e5ad8f08ac02c572b50197e3df9eb90
Dmitry Osipov <dimaosipov00@gmail.com>
2023-12-19 19:46:44 +0300

    Added the ability to view thumbnails. Prepared a view for image likes, updated paths and templates

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 from .forms import ImageCreateForm
 from .models import Image
@@ -41,3 +43,28 @@ def image_detail(request, id, slug):
     """
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html', context={'section': 'images', 'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request):
+    """
+    Функция представления отвечает за отображение лайков и дизлайков для изображения.
+
+    :param request: Запрос клиента.
+    :return: Ответ в формате json.
+    """
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
